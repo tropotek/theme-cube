a4e72badfb606a468dcd6bed926bfd11a8c32762
tropotek <info@tropotek.com>
2021-10-18 14:12:23 +1100

    ~Auto: Commit

--- js/tk-cube.js
+++ js/tk-cube.js
@@ -154,10 +154,52 @@ $(function($) {
 			$('#nav-col-submenu').html('');
 		}
 	});
-	
+
+	/****************************************************************************/
+
+	function createCookie(name, value, days) {
+		var expires;
+
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+			expires = "; expires=" + date.toGMTString();
+		} else {
+			expires = "";
+		}
+		document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+	}
+	function readCookie(name) {
+		var nameEQ = encodeURIComponent(name) + "=";
+		var ca = document.cookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) === ' ')
+				c = c.substring(1, c.length);
+			if (c.indexOf(nameEQ) === 0)
+				return decodeURIComponent(c.substring(nameEQ.length, c.length));
+		}
+		return null;
+	}
+	function eraseCookie(name) {
+		createCookie(name, "", -1);
+	}
+	/****************************************************************************/
+
 	$('#make-small-nav').click(function (e) {
 		$('#page-wrapper').toggleClass('nav-small');
+		createCookie('nav-small', $('#page-wrapper').hasClass('nav-small'));
 	});
+	if (readCookie('nav-small') == 'true') {
+		$('#page-wrapper').addClass('nav-small');
+	} else {
+		$('#page-wrapper').removeClass('nav-small');
+	}
+
+
+
+
+
 	
 	$(window).smartresize(function(){
 		if ($( document ).width() <= 991) {
